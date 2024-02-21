d2638952d5ab56abd62f753150140a79131ed3d6
Sergey <mail@vasya.com>
2024-02-22 00:58:25 +0300

    fixed bugs, tests added

--- modules/getPath.js
+++ modules/getPath.js
@@ -1,70 +1,74 @@
 export function getPath(el) {
-  let path = "";
+  let path = el.tagName.toLowerCase();
 
   if (!(el instanceof HTMLElement)) {
-    console.log("EL ",el)
     return "";
   }
 
-  if (el.tagName === "body" || el.tagName === "html") {
-    return el.tagName;
+  if (el.tagName === "BODY" || el.tagName === "HTML") {
+    return el.tagName.toLocaleLowerCase();
   }
 
   if (el.id && el.id.match(/^[a-z].*/)) {
     return `#${el.id}`;
   }
 
-  if (el.classList.length && document.querySelectorAll(el.tagName).length > 1) {
+  if (
+    el.className.length &&
+    document.getElementsByClassName(el.className).length === 1
+  ) {
+   
     path = Array.from(el.classList).reduce(
-      (curr, next) => curr + ` .${next}`,
+      (curr, next) => curr + `.${next}`,
       ""
     );
-    if (document.querySelectorAll(el.className).length === 1) {
-      return path;
-    }
-  } else {
-    path = el.tagName;
+
+    return path;
   }
 
   const needNthPart = (el) => {
     let sibling = el.previousElementSibling;
-    if (!el.className){
-        return true;
-    }
 
-    while (sibling) {
+    if (sibling) {
+      if (!el.className) {
+        return true;
+      }
+
+      while (sibling) {
         if (el.className !== sibling.className) {
-            return false
+          return false;
         }
 
         sibling = sibling.previousElementSibling;
+      }
+
+      return true;
+
+    } else {
+      return false;
     }
-
-    return false
-
-  }
+  };
 
   const getNthPart = (el) => {
     let childIndex = 1;
 
     let sibling = el.previousElementSibling;
-    
-    while(sibling) {
-        childIndex ++;
-        sibling = sibling.previousElementSibling;
+
+    while (sibling) {
+      childIndex++;
+      sibling = sibling.previousElementSibling;
     }
 
-    return `:nth-child(${childIndex})`
-  }
+    return `:nth-child(${childIndex})`;
+  };
 
   if (needNthPart(el)) {
     path += getNthPart(el);
   }
 
-
   if (!el.parentElement) {
     return path;
   }
 
-  return `${getPath(el.parentElement)} > ${path}`
+  return `${getPath(el.parentElement)} > ${path}`;
 }
